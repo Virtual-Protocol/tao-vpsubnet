04dabdecca6c0193430ecfe3546dd014921e90dc
kw <kahwai@pathdao.io>
2024-04-27 13:57:05 +0800

    revert

--- scripts/start_validator.sh
+++ scripts/start_validator.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="$PWD:$PWD/vpa2a"
-export VALIDATOR_LIB=https://gyupnvds3t.ap-southeast-1.awsapprunner.com/challenges
 python neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9944 --wallet.name validator --wallet.hotkey default --logging.debug
