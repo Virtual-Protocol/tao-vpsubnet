5befc4fc62a8f65ec4d66303569765bbcf8c8197
kw <kahwai@pathdao.io>
2024-04-30 21:16:19 +0800

    we should give sufficient time for miner to do the inference

--- vpa2a/validator/forward.py
+++ vpa2a/validator/forward.py
@@ -62,15 +62,11 @@ async def forward(self):
 
         # The dendrite client queries the network.
         responses = await self.dendrite(
-            # Send the query to selected miner axons in the network.
             axons=[self.metagraph.axons[uid] for uid in miner_uids],
-            # Construct a dummy query. This simply contains a single integer.
             synapse=synapse,
-            # All responses have the deserialize function called on them before returning.
-            # You are encouraged to define your own deserialization function.
+            timeout=300,
             deserialize=True,
         )
-
         # Write responses to disk
         response_paths = []
         for idx, response in enumerate(responses):
