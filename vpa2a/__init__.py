9bb35f03966d2f840a269dd51cad22ca808734df
kw <kahwai@pathdao.io>
2024-05-08 12:07:01 +0800

    bump spec version and remove previous workaround

--- vpa2a/__init__.py
+++ vpa2a/__init__.py
@@ -1,10 +1,11 @@
-__version__ = "0.0.1"
+__version__ = "0.4.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
     + (10 * int(version_split[1]))
     + (1 * int(version_split[2]))
 )
+
 
 # Import all submodules.
 from . import protocol
